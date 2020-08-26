a3b77084cd1ebb374fcb6354c54271108852b0bb
MatthiasG42 <matthias.gudernatsch@gmx.de>
2020-08-26 15:57:38 +0200

    Style changes

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@
  * 
 */
 
-// arrange all section in the document into a list
+// out together all sections in the document into a list
 const sections = document.querySelectorAll('section');
 // search for the element where the navbar will be stored into
 const navMenu = document.getElementById('navbar__list');
@@ -54,28 +54,29 @@ const navList = document.querySelectorAll('.menu-link');
 // Looking for the section that is closest to the top of the Viewport and then change that one to active
 // while setting all the others not active
 const activation = () =>
-{
-    let minimum = sections[0];
-    sections.forEach(element => 
-        {
-            if (Math.abs(element.getBoundingClientRect().top) <= Math.abs(minimum.getBoundingClientRect().top))
-                {
-                    minimum = element;
-                };
-            element.classList.remove('your-active-class');
-        });
-    minimum.classList.add('your-active-class');
-        
-    // Setting the Link "Button" to active in the background if the Section Name matches
-    navList.forEach(element =>
-        {
-            if (minimum.id == element.dataset.link)
-                    element.classList.add('active');
-                else
-                    element.classList.remove('active');
-        });
-};
+    {
+        let minimum = sections[0];
+        sections.forEach(element => 
+            {
+                if (Math.abs(element.getBoundingClientRect().top) <= Math.abs(minimum.getBoundingClientRect().top))
+                    {
+                        minimum = element;
+                    };
+                element.classList.remove('your-active-class');
+            });
+        minimum.classList.add('your-active-class');
+            
+        // Setting the Link "Button" to active in the background if the Section Name matches
+        navList.forEach(element =>
+            {
+                if (minimum.id == element.dataset.link)
+                        element.classList.add('active');
+                    else
+                        element.classList.remove('active');
+            });
+    };
 
+// running the check for activation on every 'scroll' event
 document.addEventListener('scroll', activation);
 
 // Scroll to anchor ID using scrollTO event
